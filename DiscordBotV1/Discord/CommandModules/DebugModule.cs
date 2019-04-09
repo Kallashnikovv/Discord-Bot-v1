3d27b66dc6d73ddeed2a63a8e478a4bd5dc9d444
Igor <xigorziomx@gmail.com>
2019-04-09 20:28:12 +0200

    Added more commands.
    Added debug and fun command modules.

---
+++ DiscordBotV1/Discord/CommandModules/DebugModule.cs
@@ -0,0 +1,20 @@
+﻿using Discord;
+using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace DiscordBotV1.Discord.CommandModules
+{
+    public class DebugModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("spam")]
+        [RequireUserPermission(GuildPermission.Administrator)]
+        public async Task Spam(int amount = 10, [Remainder]string msg = "~Spam!")
+        {
+            while(amount != 0)
+            {
+                amount--;
+                await Context.Channel.SendMessageAsync(msg);
+            }
+        }
+    }
+}
---
+++ DiscordBotV1/Discord/CommandModules/FunModule.cs
@@ -0,0 +1,14 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace DiscordBotV1.Discord.CommandModules
+{
+    public class FunModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("8ball")]
+        public async Task YesNoGame([Remainder]string question)
+        {
+
+        }
+    }
+}
