594458625552bcae52ca4845b84a8fa7c866cbad
Igor <xigorziomx@gmail.com>
2019-04-10 12:04:21 +0200

    Create ConfigModule.cs

---
+++ DiscordBotV1/Discord/CommandModules/ConfigModule.cs
@@ -0,0 +1,16 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace DiscordBotV1.Discord.CommandModules
+{
+    public class ConfigModule : ModuleBase<SocketCommandContext>
+    {
+        [Command("prefix")]
+        public async Task ChangePrefix(string prefix)
+        {
+
+        }
+
+
+    }
+}
